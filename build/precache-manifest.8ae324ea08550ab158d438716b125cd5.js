59e8bc2952be36e3776a819e90d21f41ee766753
Andrew Mayo <amayo0925@gmail.com>
2019-04-04 18:19:28 -0400

    trying  to deploy

---
+++ build/precache-manifest.8ae324ea08550ab158d438716b125cd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b819b83bfcf1074f7ca0",
+    "url": "/static/js/main.e7fe5a36.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b819b83bfcf1074f7ca0",
+    "url": "/static/css/main.91d6b6af.chunk.css"
+  },
+  {
+    "revision": "090b4e775cd151c5b72f7aa3f73bb39c",
+    "url": "/index.html"
+  }
+];
